171549852de71e8d174d5191c2c25d98178a3df2
fkaaziebu <frederickaziebu1998@gmail.com>
2023-05-16 19:16:24 +0000

    Profile Form Editing

--- client/src/scenes/profile-form/Form.jsx
+++ client/src/scenes/profile-form/Form.jsx
@@ -4,91 +4,91 @@ const RegisterForm = () => {
   return (
     <form>
       <div className="mb-4 mt-4">
-        <label for="firstName" className="form-label fs-5">
+        <label for="firstName" className="form-label fw-bold">
           First name
         </label>
         <input
           type="firstName"
           id="firstName"
-          className="form-control fs-3"
+          className="form-control fs-5 p-3 bg-light-50 border border-0"
           placeholder="First name"
         />
       </div>
       <div className="mb-4 mt-4">
-        <label for="lastName" className="form-label fs-5">
+        <label for="lastName" className="form-label fw-bold">
           Last name
         </label>
         <input
           type="text"
           id="lastName"
-          className="form-control fs-3"
+          className="form-control fs-5 p-3 bg-light-50 border border-0"
           placeholder="Last name"
         />
       </div>
       <div className="mb-4 mt-4">
-        <label for="language" className="form-label fs-5">
+        <label for="language" className="form-label fw-bold">
           Language
         </label>
         <input
           type="text"
           id="language"
-          className="form-control fs-3"
+          className="form-control fs-5 p-3 bg-light-50 border border-0"
           placeholder="English, American"
         />
       </div>
       <div className="mb-4 mt-4">
-        <label for="referralCode" className="form-label fs-5">
+        <label for="referralCode" className="form-label fw-bold">
           Referral code
         </label>
         <input
           type="text"
           id="referralCode"
-          className="form-control fs-3"
+          className="form-control fs-5 p-3 bg-light-50 border border-0"
           placeholder="Referral code"
         />
         <p>If someone referred you, enter their code</p>
       </div>
       <div className="mb-4 mt-4">
-        <label for="model" className="form-label fs-5">
+        <label for="model" className="form-label fw-bold">
           Vehicle manufacturer and model
         </label>
         <input
           type="text"
           id="model"
-          className="form-control fs-3"
+          className="form-control fs-5 p-3 bg-light-50 border border-0"
           placeholder=""
         />
       </div>
       <div className="mb-4 mt-4">
-        <label for="vehicleYear" className="form-label fs-5">
+        <label for="vehicleYear" className="form-label fw-bold">
           Vehicle year
         </label>
         <input
           type="text"
           id="vehicleYear"
-          className="form-control fs-3"
+          className="form-control fs-5 p-3 bg-light-50 border border-0"
           placeholder=""
         />
       </div>
       <div className="mb-4 mt-4">
-        <label for="licensePlate" className="form-label fs-5">
+        <label for="licensePlate" className="form-label fw-bold">
           License plate
         </label>
         <input
           type="text"
           id="licensePlate"
-          className="form-control fs-3"
+          className="form-control fs-5 p-3 bg-light-50 border border-0"
           placeholder="717 TTP"
         />
       </div>
       <div className="mb-4 mt-4">
-        <label for="color" className="form-label fs-5">
+        <label for="color" className="form-label fw-bold">
           Vehicle color
         </label>
         <input
           type="text"
           id="color"
-          className="form-control fs-3"
+          className="form-control fs-5 p-3 bg-light-50 border border-0"
           placeholder=""
         />
       </div>
