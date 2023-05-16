35f00f2bba4148cee24c04ea5505843e5f88e6e1
fkaaziebu <frederickaziebu1998@gmail.com>
2023-05-16 18:12:44 +0000

    Profile Title

--- client/src/scenes/profile-form/Form.jsx
+++ client/src/scenes/profile-form/Form.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 function Form() {
-  return <div className="container-sm d-flex justify-content-center">
-    Form
-  </div>;
+  return (
+    <div className="container-sm d-flex flex-column align-items-center mt-4">
+      <div className="w-100 mt-4">
+        <h2 className="text-sm-center fs-3">
+          Personsal information and vehicle details
+        </h2>
+        <p className="text-sm-center text-muted">
+          Only your first name and vehicle details are visible to clients during
+          the booking
+        </p>
+      </div>
+      <div>Form Fields</div>
+    </div>
+  );
 }
 
 export default Form;
