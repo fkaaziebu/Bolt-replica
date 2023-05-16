60a06fbe853cab7245dd1cbc66fccc437478458b
fkaaziebu <frederickaziebu1998@gmail.com>
2023-05-16 19:52:59 +0000

    Scroll to Signup
    On click of the Sign Up as driver button at the bottom, page scrolls to the top where sign up form is

--- client/src/scenes/home/Signup.jsx
+++ client/src/scenes/home/Signup.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
+  const navigate = useNavigate()
   return <div className="container-lg d-flex justify-content-center mb-5">
-    <button type="button" className="btn btn-primary p-3 fs-4">Sign up to drive with DMS</button>
+    <button type="button" className="btn btn-primary p-3 fs-4" onClick={() => {
+      // eslint-disable-next-line no-restricted-globals
+      scrollTo(0, 0)
+    }}>Sign up to drive with DMS</button>
   </div>;
 }
 
