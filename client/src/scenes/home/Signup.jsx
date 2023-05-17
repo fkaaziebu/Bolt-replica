96c46a92cfc2619e16318022f1e485dcdf15acbb
fkaaziebu <frederickaziebu1998@gmail.com>
2023-05-17 22:00:38 +0000

    Scroll To Signup-Updated

--- client/src/scenes/home/Signup.jsx
+++ client/src/scenes/home/Signup.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Signup() {
-  const navigate = useNavigate()
-  return <div className="container-lg d-flex justify-content-center mb-5">
-    <button type="button" className="btn btn-primary p-3 fs-4" onClick={() => {
-      // eslint-disable-next-line no-restricted-globals
-      scrollTo(0, 0)
-    }}>Sign up to drive with DMS</button>
-  </div>;
+  const navigate = useNavigate();
+  return (
+    <div className="container-lg d-flex justify-content-center mb-5">
+      <button
+        type="button"
+        className="btn btn-primary p-3 fs-4"
+        onClick={() => {
+          // eslint-disable-next-line no-restricted-globals
+          scrollTo(0, 0);
+        }}
+      >
+        Sign up to drive with DMS
+      </button>
+    </div>
+  );
 }
 
 export default Signup;
