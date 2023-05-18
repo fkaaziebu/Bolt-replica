fe0456e13e65558c642a2d72272b22f86c28bb6b
fkaaziebu <frederickaziebu1998@gmail.com>
2023-05-18 11:29:12 +0000

    Backend connection

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -11,7 +11,7 @@ const RegisterForm = () => {
   const submitRegisterInfo = async (event) => {
     event.preventDefault();
 
-    const res = await axios.post("http://localhost:7000/api/1.0/drivers", {
+    const res = await axios.post("https://dms-backend.onrender.com/api/1.0/drivers", {
       email,
       contact,
       city,
