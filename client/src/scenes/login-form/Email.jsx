1d8e387c05c22d8fbf777d52e39b636d153226d0
FaradayJnr <faradafoundationgh@gmail.com>
2023-05-25 16:57:50 +0000

    login page

--- client/src/scenes/login-form/Email.jsx
+++ client/src/scenes/login-form/Email.jsx
@@ -1,42 +1,25 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Password from "./Password";
 import { MDBTabsPane, MDBBtn, MDBInput } from "mdb-react-ui-kit";
-import axios from "axios";
+
+
 
 const Login = () => {
   const [justifyActive, setJustifyActive] = useState("tab1");
-  const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const navigate = useNavigate();
-
-  const handleSubmit = () => {
-    return <Password />;
-  };
   return (
     <MDBTabsPane show={justifyActive === "tab1"}>
-      <form onSubmit={handleSubmit}>
         <MDBInput
           wrapperClass="mb-4"
           label="Email address"
           id="form1"
           type="email"
-          name="email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-            setEmailError("");
-          }}
         />
-
-        <div className="d-flex justify-content-center mx-4 mb-4">
-          <a href="!#">Forgot password?</a>
-        </div>
-
         <MDBBtn type="submit" className="mb-4 w-100">
           Continue
         </MDBBtn>
-      </form>
+      
+        <div className="d-flex justify-content-center mx-4 mb-4">
+          <a href="!#">Forgot password?</a>
+        </div>
 
       <p className="text-center">
         Not a Bolt Driver? <a href="#!">Register</a>
--- client/src/scenes/login-form/Phone.jsx
+++ client/src/scenes/login-form/Phone.jsx
@@ -1,19 +1,24 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment } from "react";
 import {
   MDBTabsPane,
   MDBBtn,
   MDBIcon,
   MDBInput,
-  MDBCheckbox
-} from 'mdb-react-ui-kit';
+  MDBCheckbox,
+} from "mdb-react-ui-kit";
 const Phone = () => {
   return (
     <Fragment>
-    <MDBInput wrapperClass='mb-4' label='Phone' id='form1' type='contact'/>
+      <MDBInput wrapperClass="mb-4" label="Phone" id="form1" type="contact" />
+      <MDBBtn className="mb-4 w-100">Continue</MDBBtn>
 
-          <MDBBtn className="mb-4 w-100">Continue</MDBBtn>
-</Fragment>
+      <div className="d-flex justify-content-center mx-4 mb-4">
+        <a href="!#">Forgot password?</a>
+      </div>
+      <p className="text-center">
+        Not a Bolt Driver? <a href="#!">Register</a>
+      </p>
+    </Fragment>
   );
-}
+};
 export default Phone;
- 
