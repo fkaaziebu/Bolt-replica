bf6dc14cc39c00bf0a2176460afb32b03d9e04a5
FaradayJnr <faradafoundationgh@gmail.com>
2023-05-27 07:25:34 +0000

    react-mdb-login-form

--- client/src/scenes/login-form/Email.jsx
+++ client/src/scenes/login-form/Email.jsx
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
 import { MDBTabsPane, MDBBtn, MDBInput } from "mdb-react-ui-kit";
 
-const handleSubmit = () => {};
-
 const Login = () => {
   const [justifyActive, setJustifyActive] = useState("tab1");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent form submission refresh
+
+    // Perform form submission handling logic here
+
+    // Example: Access input field values
+    const email = e.target.elements.email.value;
+    const password = e.target.elements.password.value;
+
+    // Example: Log the values to the console
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
     <MDBTabsPane show={justifyActive === "tab1"}>
-      <MDBInput
-        wrapperClass="mb-4"
-        label="Email address"
-        id="form1"
-        type="email"
-      />
-      <MDBInput
-        wrapperClass="mb-4"
-        label="Password"
-        id="form1"
-        type="password"
-      />
-      <MDBBtn type="submit" className="mb-4 w-100">
-        Continue
-      </MDBBtn>
+      <form onSubmit={handleSubmit}>
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Email address"
+          id="email"
+          type="email"
+        />
+        <MDBInput
+          wrapperClass="mb-4"
+          label="Password"
+          id="password"
+          type="password"
+        />
+        <MDBBtn type="submit" className="mb-4 w-100">
+          Continue
+        </MDBBtn>
+      </form>
 
       <div className="d-flex justify-content-center mx-4 mb-4">
         <a href="!#">Forgot password?</a>
