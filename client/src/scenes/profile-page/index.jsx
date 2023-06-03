aba1fa20a7556169a4dcd8dc4d09fe1e6812c902
FaradayJnr <faradafoundationgh@gmail.com>
2023-06-03 12:34:46 +0000

    driver profile page

---
+++ client/src/scenes/profile-page/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MiniDrawer from "./DriverProfile";
+
+function Home() {
+  return (
+    <div className="container-fluid p-0">
+      <MiniDrawer />
+    </div>
+  );
+}
+
+export default Home;
